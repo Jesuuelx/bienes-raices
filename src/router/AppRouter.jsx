1ba82b79661969b1e9318e77f947d409a30bcc06
Jesus Salas <jesusalejandrosalas@hotmail.com>
2022-08-08 17:27:39 -0500

    no hice cambios

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -21,67 +21,65 @@ export const AppRouter = () => {
   const [isOpen, openModal, closeModal] = useModal(false);
 
   return (
-    <div className="container">
-      <div className="step__main">
-        <div className="step__box-container1">
-          <Routes>
+    <div className="step__main">
+      <div className="step__box-container1">
+        <Routes>
+          <Route
+            path="/nombre-usuario"
+            element={<NameStep stepOne={steps[0]} />}
+          />
+
+          {displayName !== null ? (
             <Route
-              path="/nombre-usuario"
-              element={<NameStep stepOne={steps[0]} />}
+              path="/correo-usuario"
+              element={<EmailStep stepTwo={steps[1]} />}
             />
+          ) : (
+            <Route path="/nombre-usuario" element={<NameStep />} />
+          )}
 
-            {displayName !== null ? (
-              <Route
-                path="/correo-usuario"
-                element={<EmailStep stepTwo={steps[1]} />}
-              />
-            ) : (
-              <Route path="/nombre-usuario" element={<NameStep />} />
-            )}
+          {email !== null ? (
+            <Route
+              path="/direccion"
+              element={<AddressStep stepThree={steps[2]} />}
+            />
+          ) : (
+            <Route path="/nombre-usuario" element={<NameStep />} />
+          )}
+          {address !== null ? (
+            <Route
+              path="/datos-piso"
+              element={<FloorStep stepFour={steps[3]} />}
+            />
+          ) : (
+            <Route path="/nombre-usuario" element={<NameStep />} />
+          )}
 
-            {email !== null ? (
-              <Route
-                path="/direccion"
-                element={<AddressStep stepThree={steps[2]} />}
-              />
-            ) : (
-              <Route path="/nombre-usuario" element={<NameStep />} />
-            )}
-            {address !== null ? (
-              <Route
-                path="/datos-piso"
-                element={<FloorStep stepFour={steps[3]} />}
-              />
-            ) : (
-              <Route path="/nombre-usuario" element={<NameStep />} />
-            )}
+          {floor !== null ? (
+            <Route
+              path="/caracteristicas-adicional"
+              element={<AdditionalStep stepFive={steps[4]} />}
+            />
+          ) : (
+            <Route path="/nombre-usuario" element={<NameStep />} />
+          )}
+          {floor !== null ? (
+            <Route path="/resumen" element={<Resumen stepSix={steps[5]} />} />
+          ) : (
+            <Route path="/nombre-usuario" element={<NameStep />} />
+          )}
 
-            {floor !== null ? (
-              <Route
-                path="/caracteristicas-adicional"
-                element={<AdditionalStep stepFive={steps[4]} />}
-              />
-            ) : (
-              <Route path="/nombre-usuario" element={<NameStep />} />
-            )}
-            {floor !== null ? (
-              <Route path="/resumen" element={<Resumen stepSix={steps[5]} />} />
-            ) : (
-              <Route path="/nombre-usuario" element={<NameStep />} />
-            )}
-
-            <Route path="/*" element={<Navigate to="/nombre-usuario" />} />
-          </Routes>
-          <div>
-            <button onClick={openModal} className={"btn-modal"}>
-              Ver tu Resumen
-            </button>
-            <Modal isOpen={isOpen} closeModal={closeModal} />
-          </div>
+          <Route path="/*" element={<Navigate to="/nombre-usuario" />} />
+        </Routes>
+        <div>
+          <button onClick={openModal} className={"btn-modal"}>
+            Ver tu Resumen
+          </button>
+          <Modal isOpen={isOpen} closeModal={closeModal} />
         </div>
-        <div className="step__box-step mt-5">
-          <Progress />
-        </div>
+      </div>
+      <div className="step__box-step mt-5">
+        <Progress />
       </div>
     </div>
   );
