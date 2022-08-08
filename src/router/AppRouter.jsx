d0c9f146177b44cab8ce4af3845419b2c6ea6e71
Jesus Salas <jesusalejandrosalas@hotmail.com>
2022-08-08 17:05:05 -0500

    un simple estilo de css mas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -21,56 +21,67 @@ export const AppRouter = () => {
   const [isOpen, openModal, closeModal] = useModal(false);
 
   return (
-    <div className="step__main">
-      <div className="step__box-container1">
-        <Routes>
-          <Route
-            path="/nombre-usuario"
-            element={<NameStep stepOne={steps[0]} />}
-          />
-
-          {displayName !== null ? (
-            <Route path="/correo-usuario" element={<EmailStep stepTwo={steps[1]} />} />
-          ) : (
-            <Route path="/nombre-usuario" element={<NameStep />} />
-          )}
-
-          {email !== null ? (
-            <Route path="/direccion" element={<AddressStep stepThree={ steps[2] } />} />
-          ) : (
-            <Route path="/nombre-usuario" element={<NameStep />} />
-          )}
-          {address !== null ? (
-            <Route path="/datos-piso" element={<FloorStep stepFour={ steps[3] } />} />
-          ) : (
-            <Route path="/nombre-usuario" element={<NameStep />} />
-          )}
-
-          {floor !== null ? (
+    <div className="container">
+      <div className="step__main">
+        <div className="step__box-container1">
+          <Routes>
             <Route
-              path="/caracteristicas-adicional"
-              element={<AdditionalStep stepFive={steps[4]} />}
+              path="/nombre-usuario"
+              element={<NameStep stepOne={steps[0]} />}
             />
-          ) : (
-            <Route path="/nombre-usuario" element={<NameStep />} />
-          )}
-          {floor !== null ? (
-            <Route path="/resumen" element={<Resumen stepSix={steps[5]} />} />
-          ) : (
-            <Route path="/nombre-usuario" element={<NameStep />} />
-          )}
 
-          <Route path="/*" element={<Navigate to="/nombre-usuario" />} />
-        </Routes>
-        <div>
-          <button onClick={openModal} className={"btn-modal"}>
-            Ver tu Resumen
-          </button>
-          <Modal isOpen={isOpen} closeModal={closeModal} />
+            {displayName !== null ? (
+              <Route
+                path="/correo-usuario"
+                element={<EmailStep stepTwo={steps[1]} />}
+              />
+            ) : (
+              <Route path="/nombre-usuario" element={<NameStep />} />
+            )}
+
+            {email !== null ? (
+              <Route
+                path="/direccion"
+                element={<AddressStep stepThree={steps[2]} />}
+              />
+            ) : (
+              <Route path="/nombre-usuario" element={<NameStep />} />
+            )}
+            {address !== null ? (
+              <Route
+                path="/datos-piso"
+                element={<FloorStep stepFour={steps[3]} />}
+              />
+            ) : (
+              <Route path="/nombre-usuario" element={<NameStep />} />
+            )}
+
+            {floor !== null ? (
+              <Route
+                path="/caracteristicas-adicional"
+                element={<AdditionalStep stepFive={steps[4]} />}
+              />
+            ) : (
+              <Route path="/nombre-usuario" element={<NameStep />} />
+            )}
+            {floor !== null ? (
+              <Route path="/resumen" element={<Resumen stepSix={steps[5]} />} />
+            ) : (
+              <Route path="/nombre-usuario" element={<NameStep />} />
+            )}
+
+            <Route path="/*" element={<Navigate to="/nombre-usuario" />} />
+          </Routes>
+          <div>
+            <button onClick={openModal} className={"btn-modal"}>
+              Ver tu Resumen
+            </button>
+            <Modal isOpen={isOpen} closeModal={closeModal} />
+          </div>
         </div>
-      </div>
-      <div className="step__box-step mt-5">
-        <Progress />
+        <div className="step__box-step mt-5">
+          <Progress />
+        </div>
       </div>
     </div>
   );
